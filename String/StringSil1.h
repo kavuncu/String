b88b866f653c0311203f5996f06c93f7992c63b9
mustafa <mustafaayan@gmail.com>
2012-12-06 00:19:50 +0200

    sil1 eklendi

---
+++ String/StringSil1.h
@@ -0,0 +1,13 @@
+//
+//  StringSil1.h
+//  String
+//
+//  Created by mustafa ayan on 12/6/12.
+//  Copyright (c) 2012 Rotasoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StringSil1 : NSObject
+
+@end
