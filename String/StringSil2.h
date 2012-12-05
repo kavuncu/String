4250237dad6a51b5dd4076e2b2f5785485054819
mustafa <mustafaayan@gmail.com>
2012-12-06 00:19:25 +0200

    sil2 eklendi

---
+++ String/StringSil2.h
@@ -0,0 +1,13 @@
+//
+//  StringSil2.h
+//  String
+//
+//  Created by mustafa ayan on 12/6/12.
+//  Copyright (c) 2012 Rotasoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StringSil2 : NSObject
+
+@end
