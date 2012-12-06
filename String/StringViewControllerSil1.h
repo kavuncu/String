4a226509eb713ef908e5925d7a3a02dced24921f
mustafa <mustafaayan@gmail.com>
2012-12-06 16:47:31 +0200

    birinci hub

---
+++ String/StringViewControllerSil1.h
@@ -0,0 +1,13 @@
+//
+//  StringViewControllerSil1.h
+//  String
+//
+//  Created by mustafa ayan on 12/6/12.
+//  Copyright (c) 2012 Rotasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StringViewControllerSil1 : UIViewController
+
+@end
---
+++ String/StringViewControllerSil1.m
@@ -0,0 +1,38 @@
+//
+//  StringViewControllerSil1.m
+//  String
+//
+//  Created by mustafa ayan on 12/6/12.
+//  Copyright (c) 2012 Rotasoft. All rights reserved.
+//
+
+#import "StringViewControllerSil1.h"
+
+@interface StringViewControllerSil1 ()
+
+@end
+
+@implementation StringViewControllerSil1
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
